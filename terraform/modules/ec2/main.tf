4791dc176eb7359a2cf49e2583b62337a7915b3c
YacqubAdan <yacquubadan.1@gmail.com>
2025-02-06 23:58:48 +0000

    attached public ip

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -42,6 +42,7 @@ resource "aws_instance" "wordpress_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = var.public_subnet_id
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.wordpress_sg.id]
   key_name               = var.key_name
 
